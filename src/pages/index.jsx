c4bf066cb70a690d9fef1a364c0ca0cbdeca6eb8
sudaiskhanDev <sudaisliveofficial@gmail.com>
2025-12-01 16:40:04 +0500

    Integrate Navbar and Header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
+import Header from '@/components/Header'
 
 export default function index() {
   return (
     <>
-    
+    <Navbar/>
+    <Header/>
     </>
   )
 }
